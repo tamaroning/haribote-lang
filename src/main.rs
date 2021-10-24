0c1840894febc0fa659a7f31e9a7e7aa375b2100
tamaron <tamaron1203@gmail.com>
2021-10-25 01:18:40 +0900

    clean source code

--- src/main.rs
+++ src/main.rs
@@ -57,18 +57,16 @@ fn is_normal_symbol(c: char) -> bool {
 }
 
 #[derive(Debug)]
-struct Lexer<'a> {
-    txt: &'a String,
+struct Lexer {
+    txt: String,
     pos: usize,
-    tokens: Vec<Token<'a>>,
 }
 
-impl<'a> Lexer<'a> {
-    fn new(prg: &'a String) -> Self {
+impl Lexer {
+    fn new(prg: String) -> Self {
         Lexer {
             txt: prg,
             pos: 0,
-            tokens: Vec::new(),
         }
     }
  
@@ -80,7 +78,8 @@ impl<'a> Lexer<'a> {
         self.txt[self.pos..].chars().nth(n).unwrap()
     }
 
-    fn lex<'b>(&mut self) {
+    fn lex(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
         while self.pos < self.txt.len() {
             let start_pos = self.pos;
             if is_whitespace(self.next_char()) {
@@ -103,16 +102,22 @@ impl<'a> Lexer<'a> {
                 println!("Syntax error : '{}'", self.next_char());
                 std::process::exit(0);
             }
-            let s: &'a str = &self.txt[start_pos .. self.pos];
+            let s: &str = &self.txt[start_pos .. self.pos];
             //println!("{:?}", Token::new(s));
-            self.tokens.push(Token::new(s));
+            tokens.push(Token::new(s));
         }
+        tokens.push(Token::new(";"));
+        tokens.push(Token::new("."));
+        tokens.push(Token::new("."));
+        tokens.push(Token::new("."));
+        tokens
     }
 }
 
 struct Parser<'a> {
     tokens: Vec<Token<'a>>,
     pos: usize,
+    internal_code: [Option<&'a Token<'a>>; 8],
 }
 
 impl<'a> Parser<'a> {
@@ -120,12 +125,15 @@ impl<'a> Parser<'a> {
         Parser {
             tokens: token_vec,
             pos: 0,
+            internal_code: [None; 8],
         }
     }
 
     fn phrase_compare(&self, phr: Vec<&str>) -> bool {
         for i in 0..phr.len() {
-            if phr[i] == "*" {
+            if phr[i].starts_with("*") {
+                let n = phr[i][1..].parse::<usize>().unwrap();
+                //INTERNAL_CODE[n] = Some(&self.tokens[self.pos + i]);
                 continue;
             } else if !self.tokens[self.pos + i].matches(phr[i]) {
                 return false;
@@ -146,6 +154,7 @@ impl VariableMap {
         }
     }
 
+    // TODO: to_string() is a bottleneck
     fn get(&mut self, tok: &Token) -> i32 {
         if self.map.contains_key(tok.string) {
             return *self.map.get(tok.string).unwrap();
@@ -166,23 +175,19 @@ impl VariableMap {
         }
     }
 
+    // TODO: to_string() is a bottleneck
     fn set(&mut self, tok: &Token, val: i32) {
         self.map.insert(tok.string.to_string(), val);
     }
 }
 
-fn run<'a>(s: &String, var: &mut VariableMap) {
+fn run(s: String, var: &mut VariableMap) {
     let t0 = unsafe {
         ffi::clock()
     };
+    
     let mut lexer = Lexer::new(s);
-    lexer.lex();
-
-    let mut tokens = lexer.tokens;
-    tokens.push(Token::new(";"));
-    tokens.push(Token::new("."));
-    tokens.push(Token::new("."));
-    tokens.push(Token::new("."));
+    let tokens = lexer.lex();
 
     // register labels
     for pc in 0..tokens.len() - 3 {
@@ -195,38 +200,38 @@ fn run<'a>(s: &String, var: &mut VariableMap) {
 
     while parser.pos < parser.tokens.len() - 3 {
         // assignment
-        if parser.phrase_compare(vec!["*", "=", "*", ";"]) {
+        if parser.phrase_compare(vec!["*0", "=", "*1", ";"]) {
             let val = var.get(&parser.tokens[parser.pos + 2]);
             var.set(&parser.tokens[parser.pos], val);
         }
         // add
-        else if parser.phrase_compare(vec!["*", "=", "*", "+", "*", ";"]) {
+        else if parser.phrase_compare(vec!["*0", "=", "*1", "+", "*2", ";"]) {
             let lhs = var.get(&parser.tokens[parser.pos + 2]);
             let rhs = var.get(&parser.tokens[parser.pos + 4]);
             var.set(&parser.tokens[parser.pos], lhs + rhs);
         }
         // subtract
-        else if parser.phrase_compare(vec!["*", "=", "*", "-", "*", ";"]) {
+        else if parser.phrase_compare(vec!["*0", "=", "*1", "-", "*2", ";"]) {
             let lhs = var.get(&parser.tokens[parser.pos + 2]);
             let rhs = var.get(&parser.tokens[parser.pos + 4]);
             var.set(&parser.tokens[parser.pos], lhs - rhs);
         }
         // print
-        else if parser.phrase_compare(vec!["print", "*", ";"]) {
+        else if parser.phrase_compare(vec!["print", "*0", ";"]) {
             println!("{}", var.get(&parser.tokens[parser.pos + 1]));
         }
         // label
-        else if parser.phrase_compare(vec!["*", ":"]) {
+        else if parser.phrase_compare(vec!["*0", ":"]) {
             parser.pos += 2;
             continue;
         }
         // goto
-        else if parser.phrase_compare(vec!["goto", "*", ";"]) {
+        else if parser.phrase_compare(vec!["goto", "*0", ";"]) {
             parser.pos = var.get(&parser.tokens[parser.pos + 1]) as usize;
             continue;
         }
         // if (v0 op v1) goto label;
-        else if parser.phrase_compare(vec!["if", "(", "*", "*", "*", ")","goto", "*", ";"]) {
+        else if parser.phrase_compare(vec!["if", "(", "*0", "*1", "*2", ")","goto", "*3", ";"]) {
             let gpc = var.get(&parser.tokens[parser.pos + 7]) as usize;
             let v0 = var.get(&parser.tokens[parser.pos + 2]);
             let v1 = var.get(&parser.tokens[parser.pos + 4]);
@@ -278,7 +283,7 @@ fn main() {
     if args.len() == 2 {
         let filepath = &args[1];
         let src = load_text(filepath);
-        run(&src, &mut var);
+        run(src, &mut var);
     }
 
     // REPL
@@ -299,10 +304,10 @@ fn main() {
             else if input.starts_with("run") {
                 let filepath = &input[4..];
                 let src = load_text(filepath);
-                run(&src, &mut var);
+                run(src, &mut var);
             }
             else {
-                run(&input, &mut var);
+                run(input, &mut var);
             }
         }
     }
