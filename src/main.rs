aaa7eb33ce4fa17913dfa12f944c6575a4c5ae01
tamaron <tamaron1203@gmail.com>
2021-10-29 21:14:55 +0900

    to make it possible to parse expressions

--- src/main.rs
+++ src/main.rs
@@ -103,7 +103,6 @@ impl Lexer {
             let s = self.txt[start_pos..self.pos].to_string();
             self.tokens.push(Token::new(s));
         }
-        self.tokens.push(Token::new(String::from(";")));
         self.tokens.push(Token::new(String::from(".")));
         self.tokens.push(Token::new(String::from(".")));
         self.tokens.push(Token::new(String::from(".")));
@@ -121,7 +120,6 @@ enum Operation {
     Jeq(Token, Token, Token),
     Jne(Token, Token, Token),
     Jlt(Token, Token, Token),
-    Nop,
 }
 
 struct VariableMap {
@@ -166,7 +164,15 @@ struct Compiler {
     pos: usize,
     lexer: Lexer,
     internal_code: Vec<Operation>,
-    cur_inst_param: [Option<Token>; 8],
+    // token length of the current instruction 
+    cur_inst_len: usize,
+    // temporary strage of operation parameters
+    // see the difinition of phrase_compare
+    cur_token_param: [Option<Token>; 4],
+    cur_expr_param: [Option<Token>; 4],
+    // the number of variables which is used
+    // to store temporay results of culculation
+    temp_var_cnt: usize,
 }
 
 impl Compiler {
@@ -177,74 +183,125 @@ impl Compiler {
             pos: 0,
             lexer: lexer,
             internal_code: Vec::new(),
-            // temporary strage of operation parameters
-            // see the difinition of phrase_compare
-            cur_inst_param: Default::default(),
+            cur_inst_len: 0,
+            cur_token_param: Default::default(),
+            cur_expr_param: Default::default(),
+            temp_var_cnt: 0,
         }
+    }
+
+    fn make_temp_var(&mut self) -> Token {
+        let ret = Token::new(String::from(format!("_t{}", self.temp_var_cnt)));
+        self.temp_var_cnt += 1;
+        ret
+    }
+
+    fn primary(&mut self, start_pos: usize) -> (Token, usize) {
+        let ret = self.lexer.tokens[start_pos].clone();
+        (ret, 1)
+    }
+
+    fn add(&mut self, mut start_pos: usize) -> (Token, usize) {
+        let mut len = 0;
+        let (lhs, lhs_len) = self.primary(start_pos + len);
+        len += lhs_len;
+        let ret = self.make_temp_var();
+        
+        if self.lexer.tokens[start_pos + len].matches("+") {
+            len += 1;
+            start_pos += 1;
+            let (rhs, rhs_len) = self.add(start_pos + len);
+            len += rhs_len;
+            let op = Operation::Add(ret.clone(), lhs, rhs);
+            self.push_internal_code(op);
+        } else {
+            let op = Operation::Copy(ret.clone(), lhs);
+            self.push_internal_code(op);
+        }
+        (ret, len)
+    }
+
+    fn expr(&mut self, start_pos: usize) -> (Token, usize) {
+        self.add(start_pos)
     }
 
     fn push_internal_code(&mut self, op: Operation) {
         self.internal_code.push(op);
     }
 
+    // This function set cur_inst_len, cur_token_param, and cur_expr_param
+    // Before call this function, make sure that self.cur_inst_len=0 and that tokens[self.pos] matches the beginning of the phrase
+    // When it satisfies, tokens[self.pos + self.cur_inst_len] essentially points to the beginning of the *tXX or *eXX
     fn phrase_compare<const N: usize>(&mut self, phr: [&'static str; N]) -> bool {
+        let inst_len = self.cur_inst_len;
         for i in 0..N {
-            if phr[i].starts_with("*") {
-                let n = phr[i][1..].parse::<usize>().unwrap();
-                self.cur_inst_param[n] = Some(self.lexer.tokens[self.pos + i].clone());
+            if phr[i].starts_with("*t") {
+                let n = phr[i][2..].parse::<usize>().unwrap();
+                // TODO: this clone can be replaced something like Option::take?
+                self.cur_token_param[n] = Some(self.lexer.tokens[self.pos + self.cur_inst_len].clone());
+            } else if phr[i].starts_with("*e") {
+                let n = phr[i][2..].parse::<usize>().unwrap();
+                let (expr_tok, expr_len) = self.expr(self.pos + self.cur_inst_len);
+                self.cur_expr_param[n] = Some(expr_tok);
+                self.cur_inst_len += expr_len;
                 continue;
-            } else if !self.lexer.tokens[self.pos + i].matches(phr[i]) {
+            } else if !self.lexer.tokens[self.pos + self.cur_inst_len].matches(phr[i]) {
+                // unwind
+                self.cur_inst_len = inst_len;
                 return false;
             }
+            self.cur_inst_len += 1;
         }
         return true;
     }
 
     fn compile(&mut self, var: &mut VariableMap) {
         while self.pos < self.lexer.tokens.len() - 3 {
+            println!("instruction starts with tokens[{}]={:?}", self.pos, self.lexer.tokens[self.pos]);
+            self.cur_inst_len = 0;
             // assignment
-            if self.phrase_compare(["*0", "=", "*1", ";"]) {
-                let param0 = self.cur_inst_param[0].take().unwrap();
-                let param1 = self.cur_inst_param[1].take().unwrap();
+            if self.phrase_compare(["*t0", "=", "*t1", ";"]) {
+                let param0 = self.cur_token_param[0].take().unwrap();
+                let param1 = self.cur_token_param[1].take().unwrap();
                 self.push_internal_code(Operation::Copy(param0, param1));
             }
             // add
-            else if self.phrase_compare(["*0", "=", "*1", "+", "*2", ";"]) {
-                let param0 = self.cur_inst_param[0].take().unwrap();
-                let param1 = self.cur_inst_param[1].take().unwrap();
-                let param2 = self.cur_inst_param[2].take().unwrap();
+            else if self.phrase_compare(["*t0", "=", "*t1", "+", "*t2", ";"]) {
+                let param0 = self.cur_token_param[0].take().unwrap();
+                let param1 = self.cur_token_param[1].take().unwrap();
+                let param2 = self.cur_token_param[2].take().unwrap();
                 self.push_internal_code(Operation::Add(param0, param1, param2));
             }
             // subtract
-            else if self.phrase_compare(["*0", "=", "*1", "-", "*2", ";"]) {
-                let param0 = self.cur_inst_param[0].take().unwrap();
-                let param1 = self.cur_inst_param[1].take().unwrap();
-                let param2 = self.cur_inst_param[2].take().unwrap();
+            else if self.phrase_compare(["*t0", "=", "*t1", "-", "*t2", ";"]) {
+                let param0 = self.cur_token_param[0].take().unwrap();
+                let param1 = self.cur_token_param[1].take().unwrap();
+                let param2 = self.cur_token_param[2].take().unwrap();
                 self.push_internal_code(Operation::Sub(param0, param1, param2));
             }
             // print
-            else if self.phrase_compare(["print", "*0", ";"]) {
-                let param0 = self.cur_inst_param[0].take().unwrap();
-                self.push_internal_code(Operation::Print(param0));
+            else if self.phrase_compare(["print", "*e0", ";"]) {
+                let expr_param0 = Operation::Print(self.cur_expr_param[0].take().unwrap());
+                self.push_internal_code(expr_param0);
             }
             // label
-            else if self.phrase_compare(["*0", ":"]) {
-                let label = self.cur_inst_param[0].take().unwrap();
+            else if self.phrase_compare(["*t0", ":"]) {
+                let label = self.cur_token_param[0].take().unwrap();
                 var.set(&label, self.internal_code.len() as i32);
                 self.pos += 2;
                 continue;
             }
             // goto
-            else if self.phrase_compare(["goto", "*0", ";"]) {
-                let param0 = self.cur_inst_param[0].take().unwrap();
+            else if self.phrase_compare(["goto", "*t0", ";"]) {
+                let param0 = self.cur_token_param[0].take().unwrap();
                 self.push_internal_code(Operation::Goto(param0));
             }
             // if (v0 op v1) goto label;
-            else if self.phrase_compare(["if", "(", "*0", "*1", "*2", ")", "goto", "*3", ";"]) {
-                let label = self.cur_inst_param[3].take().unwrap();
-                let lhs = self.cur_inst_param[0].take().unwrap();
-                let rhs = self.cur_inst_param[2].take().unwrap();
-                let bin_op = &self.cur_inst_param[1].take().unwrap();
+            else if self.phrase_compare(["if", "(", "*t0", "*t1", "*t2", ")", "goto", "*t3", ";"]) {
+                let label = self.cur_token_param[3].take().unwrap();
+                let lhs = self.cur_token_param[0].take().unwrap();
+                let rhs = self.cur_token_param[2].take().unwrap();
+                let bin_op = &self.cur_token_param[1].take().unwrap();
                 if &bin_op.string == "==" {
                     self.push_internal_code(Operation::Jeq(lhs, rhs, label));
                 } else if &bin_op.string == "!=" {
@@ -266,25 +323,18 @@ impl Compiler {
                     "Syntax error: {} {} {}",
                     self.lexer.tokens[self.pos].string,
                     self.lexer.tokens[self.pos + 1].string,
-                    self.lexer.tokens[self.pos + 2].string
+                    self.lexer.tokens[self.pos + 2].string,
                 );
             }
-
-            // read forward until bumping into ";"
-            while !self.lexer.tokens[self.pos].matches(";") {
-                self.pos += 1;
-            }
-            self.pos += 1;
+            self.pos += self.cur_inst_len;
         }
     }
 
     fn exec(&self, var_map: &mut VariableMap) {
         let t0 = unsafe { ffi::clock() };
 
-        //println!("IC: {:?}", self.internal_code);
         let mut pc = 0;
         while pc < self.internal_code.len() {
-            //println!("pos: {}, IC: {:?}", pc, self.internal_code[pc]);
             match self.internal_code[pc] {
                 Operation::Copy(ref dist, ref var) => {
                     let val = var_map.get(var);
@@ -335,7 +385,6 @@ impl Compiler {
                 Operation::Time => unsafe {
                     println!("time: {}", ffi::clock() - t0);
                 },
-                Operation::Nop => (),
             }
             pc += 1;
         }
